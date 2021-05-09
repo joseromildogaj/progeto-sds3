8116a7b8a7c2be13460ad19b09d128b53012033b
José Romildo <pronuno1@gmail.com>
2021-05-09 00:00:04 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 //import DonutChar from 
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Char from "react-apexcharts";
 import { Salesum } from "Types/sale";
 import { BASE_URL } from "Utils/requests";
@@ -11,36 +12,30 @@ type ChartData = {
 
 const DonutChar = () => {
 
-    let chartData : ChartData = {labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as Salesum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+                setChartData ({ labels: myLabels, series: mySeries });
+            });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as Salesum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    }, []);
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        } );
-
-   // const mockData = {
-   //     series: [477138, 499928, 444867, 220426, 473088],
-   //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-   // }
-    
     const options = {
         legend: {
             show: true
         }
     }
     return (
-        <Char 
-            options = {{...options, labels: chartData.labels}}
-            series = {chartData.series}
-            type = "donut"
-            height = "240"
+        <Char
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
         />
     );
 }
-
 export default DonutChar;
